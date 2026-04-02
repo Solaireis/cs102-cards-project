8297e3b042bee015fe5a024aaf67e6f65043b06b
Eden <64953425+Solaireis@users.noreply.github.com>
2026-04-02 22:11:16 +0800

    test

--- Splendor/compileJavaFXEden.sh
+++ Splendor/compileJavaFXEden.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 javac --module-path /Users/edensng/javafx-sdk-26/lib  --add-modules javafx.controls,javafx.fxml -d classes $(find src -name "*.java")
 cp -a src/UI/ classes/UI/
+java --module-path /Users/edensng/javafx-sdk-26/lib --add-modules javafx.controls,javafx.fxml --enable-native-access=javafx.graphics -cp classes UI.GameApp
--- Splendor/runJavaFX.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java --module-path /Users/edensng/javafx-sdk-26/lib --add-modules javafx.controls,javafx.fxml --enable-native-access=javafx.graphics -cp classes UI.GameApp
